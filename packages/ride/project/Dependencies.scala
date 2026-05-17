4fe35197e92e98cee40e9805614eec3ff64355a9
jourlez <josuecr.288@gmail.com>
2026-05-17 07:34:04 -0600

    chore(DCC-252,DCC-260,DCC-261,DCC-242): deep production readiness audit — version standardization + sbt build fixes
    ride:
    - add sbt-assembly 2.3.1 (was missing, causing build load failure)
    - add circe-core + circe-parser 0.14.15 to shared repl deps (was relying on transitive from full node-scala build)
    - fix git version: suppress gitDescribedVersion to prevent upstream slash-tags
    - add Resolver.mavenLocal for locally-installed DCC JARs
    - scala 3.8.1 (consistent with node-scala)
    - gProtoVersion 4.33.5 -> 4.34.1
    - web3j 4.13.0 -> 5.0.2
    - mockito 5.14.2 -> 5.23.0
    - scalatestplus scalacheck-1-16:3.2.14.0 -> scalacheck-1-18:3.2.19.0
    - curve25519-java: com.wavesplatform:0.6.6 -> io.decentralchain:1.0.0 (DCC-260)
    - zwaves: com.wavesplatform:0.2.1 -> io.decentralchain:0.2.1.0 (DCC-261)
    - fix Scala 3.8.1 implicit ambiguity: move Decoder[A] givens to model companions,
      explicit getEntity using params, local given ec in Future methods
    
    jvm packages:
    - assertj 3.27.3 -> 3.27.7 (curve25519, blst, zwaves)
    - maven-source-plugin 3.3.1 -> 3.4.0 (curve25519, blst, zwaves, transactions-java)
    - Maven wrapper 3.9.9 -> 3.9.15 (blst-java, wavesj, zwaves)

--- packages/ride/project/Dependencies.scala
+++ packages/ride/project/Dependencies.scala
@@ -7,7 +7,7 @@ import scalapb.compiler.Version.scalapbVersion
 // Only includes dependencies needed by lang/ and repl/ subprojects.
 object Dependencies {
 
-  val gProtoVersion = "4.33.5"
+  val gProtoVersion = "4.34.1"
   val gProto        = "com.google.protobuf" % "protobuf-java" % gProtoVersion
 
   val overrides = Def.setting(
@@ -21,14 +21,14 @@ object Dependencies {
   lazy val protoSchemasLib =
     "io.decentralchain" % "protobuf-schemas" % "1.6.1" classifier "protobuf-src" intransitive ()
 
-  private def web3jModule(module: String) = "org.web3j" % module % "4.13.0"
+  private def web3jModule(module: String) = "org.web3j" % module % "5.0.2"
 
   def monixModule(module: String): Def.Initialize[ModuleID] =
     Def.setting("io.monix" %%% s"monix-$module" % "3.4.1")
 
   val googleGuava = "com.google.guava" % "guava"           % "33.5.0-jre"
   val logback     = "ch.qos.logback"   % "logback-classic" % "1.5.32"
-  val curve25519  = "com.wavesplatform" % "curve25519-java" % "0.6.6"
+  val curve25519  = "io.decentralchain" % "curve25519-java" % "1.0.0"
 
   val scalaLogging: ModuleID = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.6"
 
@@ -37,6 +37,14 @@ object Dependencies {
 
   private def sttp3Module(module: String) = "com.softwaremill.sttp.client3" %% module % "3.11.0"
   val sttp3 = sttp3Module("core")
+
+  lazy val circe: Def.Initialize[Seq[ModuleID]] = Def.setting {
+    val circeVersion = "0.14.15"
+    Seq(
+      "io.circe" %%% "circe-core",
+      "io.circe" %%% "circe-parser"
+    ).map(_ % circeVersion)
+  }
 
   def amazonCorretto(c: String): ModuleID =
     "software.amazon.cryptools" % "AmazonCorrettoCryptoProvider" % "2.5.0" classifier c
@@ -56,7 +64,7 @@ object Dependencies {
       "ch.obermuhlner"  % "big-math"   % "2.3.2",
       googleGuava,
       curve25519,
-      "com.wavesplatform" % "zwaves" % "0.2.1",
+      "io.decentralchain" % "zwaves" % "0.2.1.0",
       web3jModule("crypto").excludeAll(ExclusionRule("org.bouncycastle", "bcprov-jdk15on")),
       protoSchemasLib % "protobuf"
     ) ++ cryptoProviders
@@ -71,9 +79,9 @@ object Dependencies {
 
   lazy val test: Seq[ModuleID] = scalaTest +: Seq(
     logback,
-    "org.scalatestplus" %% "scalacheck-1-16" % "3.2.14.0",
+    "org.scalatestplus" %% "scalacheck-1-18" % "3.2.19.0",
     "org.scalacheck"    %% "scalacheck"      % "1.19.0",
-    "org.mockito"        % "mockito-core"    % "5.14.2",
+    "org.mockito"        % "mockito-core"    % "5.23.0",
     "org.scalamock"     %% "scalamock"       % "6.2.0"
   ).map(_ % Test)
 
--- packages/ride/repl/shared/src/main/scala/com/decentralchain/lang/v1/repl/node/http/response/model/AddressResponse.scala
+++ packages/ride/repl/shared/src/main/scala/com/decentralchain/lang/v1/repl/node/http/response/model/AddressResponse.scala
@@ -1,3 +1,12 @@
 package com.decentralchain.lang.v1.repl.node.http.response.model
 
+import io.circe.{Decoder, HCursor}
+
 private[node] case class AddressResponse(address: ByteString)
+
+private[node] object AddressResponse {
+  implicit val decoder: Decoder[AddressResponse] = (c: HCursor) =>
+    for {
+      address <- c.downField("address").as[ByteString]
+    } yield AddressResponse(address)
+}
