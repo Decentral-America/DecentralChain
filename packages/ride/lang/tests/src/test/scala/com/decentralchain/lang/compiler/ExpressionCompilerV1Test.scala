356498c6b7dc97f7d1926fbf99da9f0b31e13237
jourlez <josuecr.288@gmail.com>
2026-05-22 22:37:55 -0600

    fix(ride): align import selector ordering with scalafmt SortImports
    scalafmt's SortImports rule uses symbols-first then lowercase-before-uppercase
    ordering, which differs from scalafix OrganizeImports' default Ascii order.
    Running both tools was causing a conflict where each tool would reformat what
    the other had just fixed.
    
    Fix:
    - Add OrganizeImports.importSelectorsOrder = Keep to .scalafix.conf so scalafix
      defers selector ordering to scalafmt
    - Reformat the 10 affected Scala files via scalafmt to match the expected order
      (verified with scalameta/scalafmt:v3.11.1 Docker image, 215 source files pass)

--- packages/ride/lang/tests/src/test/scala/com/decentralchain/lang/compiler/ExpressionCompilerV1Test.scala
+++ packages/ride/lang/tests/src/test/scala/com/decentralchain/lang/compiler/ExpressionCompilerV1Test.scala
@@ -24,7 +24,7 @@ import com.decentralchain.lang.v1.parser.Parser.LibrariesOffset
 import com.decentralchain.lang.v1.parser.Parser.LibrariesOffset.NoLibraries
 import com.decentralchain.lang.v1.parser.{Expressions, Parser}
 import com.decentralchain.lang.v1.traits.Environment
-import com.decentralchain.lang.v1.{ContractLimits, FunctionHeader, compiler}
+import com.decentralchain.lang.v1.{compiler, ContractLimits, FunctionHeader}
 import com.decentralchain.lang.{Common, Global}
 import com.decentralchain.test.*
 
