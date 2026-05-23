f70929b9945eaa433bab0089eed14a24d3d31b13
Josue Rojas <josuecr.288@gmail.com>
2026-05-23 06:51:05 -0600

    fix(java-sdk): sort imports alphabetically to satisfy Checkstyle CustomImportOrder
    After renaming com.decentralchain.* → io.decentralchain.*, 31 main source files
    had io.decentralchain.transactions.* imports appearing before io.decentralchain.sdk.*
    imports. Checkstyle CustomImportOrder requires alphabetical order within the
    THIRD_PARTY_PACKAGE group (sdk < transactions alphabetically). Fixed all 31 files.

--- packages/jvm/java-sdk/src/main/java/io/decentralchain/sdk/info/GenesisTransactionInfo.java
+++ packages/jvm/java-sdk/src/main/java/io/decentralchain/sdk/info/GenesisTransactionInfo.java
@@ -1,7 +1,7 @@
 package io.decentralchain.sdk.info;
 
-import io.decentralchain.transactions.GenesisTransaction;
 import io.decentralchain.sdk.ApplicationStatus;
+import io.decentralchain.transactions.GenesisTransaction;
 
 public class GenesisTransactionInfo extends TransactionInfo {
 
