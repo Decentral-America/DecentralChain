29b0358b9658dee61cdc44ded7dda8bb0ad667ca
Josué Rojas <josuecr.288@gmail.com>
2026-06-05 22:17:04 -0600

    fix(java-sdk): replace static imports with qualified names in DccEthConverter

--- packages/jvm/java-sdk/src/main/java/io/decentralchain/sdk/util/DccEthConverter.java
+++ packages/jvm/java-sdk/src/main/java/io/decentralchain/sdk/util/DccEthConverter.java
@@ -1,15 +1,11 @@
 package io.decentralchain.sdk.util;
 
-import static io.decentralchain.crypto.Hash.blake;
-import static io.decentralchain.crypto.Hash.keccak;
-import static io.decentralchain.crypto.base.Base58.encode;
-import static java.util.Arrays.copyOfRange;
-import static org.bouncycastle.util.encoders.Hex.decode;
-
+import io.decentralchain.crypto.Hash;
 import io.decentralchain.crypto.base.Base58;
 import io.decentralchain.sdk.Node;
 import io.decentralchain.sdk.exceptions.NodeException;
 import java.io.IOException;
+import java.util.Arrays;
 import org.bouncycastle.util.encoders.Hex;
 import org.web3j.utils.Numeric;
 
@@ -27,21 +23,21 @@ public class DccEthConverter {
 
   public static String dccToEthAddress(String address) {
     byte[] dccAddress = Base58.decode(address);
-    byte[] ethAddress = copyOfRange(dccAddress, 2, 22);
+    byte[] ethAddress = Arrays.copyOfRange(dccAddress, 2, 22);
     return Numeric.toHexString(ethAddress);
   }
 
   public static String ethToDccAddress(String address, byte chainId) {
-    byte[] pkHash = copyOfRange(decode(address.substring(2)), 0, 20);
+    byte[] pkHash = Arrays.copyOfRange(Hex.decode(address.substring(2)), 0, 20);
     byte[] prefixBytes = new byte[] {0x01, chainId};
     byte[] checkSumBytes = concat(prefixBytes, pkHash);
-    byte[] checkSum = keccak(blake(checkSumBytes));
-    byte[] dccBytes = concat(concat(prefixBytes, pkHash), copyOfRange(checkSum, 0, 4));
-    return encode(dccBytes);
+    byte[] checkSum = Hash.keccak(Hash.blake(checkSumBytes));
+    byte[] dccBytes = concat(concat(prefixBytes, pkHash), Arrays.copyOfRange(checkSum, 0, 4));
+    return Base58.encode(dccBytes);
   }
 
   public static String dccToEthAsset(String asset) {
-    byte[] decode = copyOfRange(Base58.decode(asset), 0, 20);
+    byte[] decode = Arrays.copyOfRange(Base58.decode(asset), 0, 20);
     return "0x" + Hex.toHexString(decode);
   }
 
